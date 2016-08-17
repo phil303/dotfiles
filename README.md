96c9def9cc72bea84309e259a7d2f9f63e1a344c
Phil Aquilina <philaquilina@gmail.com>
2016-08-16 21:32:07 -0700

    remove ridiculous ruby script

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
     git clone git://github.com/phil303/dotfiles.git ~/.dotfiles
     cd ~/.dotfiles
     # install dotfiles to home directory
-    rake install
+    python install.py
     mkdir vim/bundle
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
     chsh -s /bin/zsh
---
+++ install.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python2.7
+import os
+
+SYMLINKS = [
+    'gitignore',
+    'zshrc',
+    'vimrc',
+]
+
+GIT_CONFIG_NAME = "Phil Aquilina"
+GIT_CONFIG_EMAIL = "philaquilina@gmail.com"
+
+HOME_DIR = os.path.expanduser('~')
+
+def install():
+    create_symlinks()
+    create_gitconfig()
+    print("Finished installing dotfiles")
+
+
+def create_symlinks():
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    for symlink in SYMLINKS:
+        local_path = "%s/%s" % (current_dir, symlink)
+
+        if not os.path.exists(local_path):
+            print("%s does not exist" % symlink)
+            continue
+
+        sym_link_path = "%s/.%s" % (HOME_DIR, symlink)
+        if os.path.exists(sym_link_path):
+            os.remove(sym_link_path)
+
+        print("Writing symlink for %s" % symlink)
+        os.symlink(local_path, sym_link_path)
+
+
+def create_gitconfig():
+    git_config_path = "%s/.gitconfig" % HOME_DIR
+
+    if os.path.exists(git_config_path):
+        reply = raw_input("gitconfig already exists. Overwrite? ")
+        overwrite = len(reply) > 0 and reply[0].lower() == "y"
+        if not overwrite:
+            return
+
+    with open(git_config_path, 'w') as f:
+        print("Writing gitconfig")
+        f.write(GIT_CONFIG)
+
+
+GIT_CONFIG = """[user]
+  name = {name}
+  email = {email}
+[alias]
+  co = checkout
+  ci = commit
+  st = status
+  br = branch
+  hist = log --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd, %cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=local
+[color]
+  diff = auto
+  status = auto
+  branch = auto
+[core]
+  editor = vim
+  autocrlf = input
+[apply]
+  whitespace = warn
+[format]
+  pretty = %C(yellow)%h%Creset %s %C(red)(%an, %cr)%Creset
+[credential]
+  helper = osxkeychain
+[diff]
+  tool = opendiff
+[push]
+  default = simple
+[filter "lfs"]
+  clean = git-lfs clean %f
+  smudge = git-lfs smudge %f
+  required = true
+""".format(name=GIT_CONFIG_NAME, email=GIT_CONFIG_EMAIL)
+
+
+
+if __name__ == '__main__':
+    install()
